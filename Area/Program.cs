4490d434807ef6bbd324c7ff3afb3ec56b1f3f9a
Mr Sticky <j.schober93@gmail.com>
2017-11-09 18:28:13 -0600

    Running fine

---
+++ Area/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the radius of the circle to calculate the area?: ");
+            string radiusString = Console.ReadLine();
+            double radius = Convert.ToDouble(radiusString);
+            double pi = 3.14;
+            double a = pi * radius * radius;
+            Console.WriteLine("The area of a circle with a radius of " + radius + " is " + a);
+            Console.ReadLine();
+                
+        }
+    }
+}
